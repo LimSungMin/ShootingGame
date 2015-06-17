e6a8e65c089a402be2bbda6df0b5c92551a1fde3
임성민 <lsmorigin@gmail.com>
2015-06-17 23:32:57 +0900

    좌,우 이동할 때 회전 추가

---
+++ Assets/test/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	float speed = 50f;
+	float tilt = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		float dirX = Input.GetAxis ("Horizontal");	
+		float dirY = Input.GetAxis ("Vertical");	
+
+		Vector3 movement = new Vector3 (dirX, 0, dirY);
+
+		GetComponent<Rigidbody> ().velocity = movement * speed;	// 방향 * 스피드
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0, 0, GetComponent<Rigidbody> ().velocity.x * -tilt);	// 회전
+        
+	}
+}
