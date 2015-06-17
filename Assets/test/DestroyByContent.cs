a0e0e1f8864309ae880f7328ed7d758f1770b930
임성민 <lsmorigin@gmail.com>
2015-06-18 03:29:37 +0900

    미사일과 적들과의 충돌처리 추가

--- Assets/test/DestroyByContent.cs
+++ Assets/test/DestroyByContent.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class DestroyByContent : MonoBehaviour
+{
+    public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")    // 적들끼리 부딫혔을때
+            return;
+
+        Instantiate(explosion, transform.position, transform.rotation);        
+        Destroy(gameObject);        // 운석
+        Destroy(other.gameObject);  // 운석과 부딫힌것
+    }
 }
