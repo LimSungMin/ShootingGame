a08e0696c1c3b977bcd8aafa67c655bbddbe9b71
임성민 <lsmorigin@gmail.com>
2015-06-22 22:49:00 +0900

    최종

--- Assets/test/DestroyByContent.cs
+++ Assets/test/DestroyByContent.cs
@@ -4,18 +4,34 @@ using System.Collections;
 public class DestroyByContent : MonoBehaviour
 {
     public GameObject explosion;
-
+    public GameObject playerExplosion;
+    GameObject GameController;
 	// Use this for initialization
 	void Start () {
-	
+        GameController = GameObject.Find("GameController");
 	}
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy")    // 적들끼리 부딫혔을때
+        {
             return;
+        }
+                    
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
 
-        Instantiate(explosion, transform.position, transform.rotation);        
+        if (other.tag == "Player")  // 만약 부딫힌게 플레이어 라면
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);   // 플레이어 위치에 파티클과 사운드 출력
+            GameController.GetComponent<GameController>().GameOver();
+        }
+
+        
+        GameController.GetComponent<GameController>().AddScore(10);
+
         Destroy(gameObject);        // 운석
         Destroy(other.gameObject);  // 운석과 부딫힌것
     }
--- Assets/test/GameController.cs
+++ Assets/test/GameController.cs
@@ -6,16 +6,53 @@ public class GameController : MonoBehaviour {
     public float m_startWait = 1;
     public float m_spawnWait = 1.5f;
     public float m_waveWait = 2;
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+    bool gameover;
+    bool restart;
+    int score;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        Screen.SetResolution(480, 800, false);
+        score = 0;
+        gameover = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        UpdateScore();
         StartCoroutine(SpawnWaves());        
 	}
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        UpdateScore();
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "GAME OVER";
+        gameover = true;
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score : "+score;
+    }
+
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(m_startWait); // m_startWait 초 대기 후 함수 작동
 
         while (true)
         {
+            if (gameover)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
             for (int i = 0; i < 10; i++)
             {
                 GameObject hazard = hazards[Random.Range(0, hazards.Length)];
@@ -27,12 +64,19 @@ public class GameController : MonoBehaviour {
             }
 
             yield return new WaitForSeconds(m_waveWait);
+
+            
         }
         
         
     }
 	// Update is called once per frame
 	void Update () {
-	
+       
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        
 	}
 }
